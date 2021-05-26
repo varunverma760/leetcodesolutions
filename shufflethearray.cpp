e8a92e66fb553aed3b52bba2ff3e6a1b25d9d290
varunverma <varunverma7.r@gmail.com>
2021-05-26 08:41:37 +0530

    shufflethearray.cpp

---
+++ shufflethearray.cpp
@@ -0,0 +1,11 @@
+class solution{
+public:
+	std::vector<<int> shuffle(vector<int>& nums ,int n){
+		std::vector<int> v;
+		for(int i=0;i<n;i++){
+			v.push_back[i];
+			v.push_back[n+i];
+		}
+		return v;
+	}
+}
